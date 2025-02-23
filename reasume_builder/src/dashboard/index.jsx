9f9245d38f43008c126cbbcbb4e81fcce7ea3c35
TekleEyesus <164241251+tekle-eyesus@users.noreply.github.com>
2025-02-23 13:39:14 -0700

    header added

--- reasume_builder/src/dashboard/index.jsx
+++ reasume_builder/src/dashboard/index.jsx
@@ -1,7 +1,12 @@
 import React from "react";
-
+import Header from "../header/index";
 function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      <Header />
+      Dashboard
+    </div>
+  );
 }
 
 export default Dashboard;
--- reasume_builder/src/header/index.jsx
+++ reasume_builder/src/header/index.jsx
@@ -6,15 +6,17 @@ import { Link } from "react-router";
 function Header() {
   const { isSignedIn } = useUser();
   return (
-    <div className='p-3 px-5 flex justify-between'>
+    <div className='p-3 px-5 flex justify-between shadow-md'>
       <img src='/logo.svg' alt='' width={100} height={100} className='' />
       {isSignedIn ? (
-        <>
-          <Button>Dashboard</Button>
+        <div className='flex items-center gap-2'>
+          <Link to={"/dashboard"}>
+            <Button variant='outline'>Dashboard</Button>
+          </Link>
           <UserButton />
-        </>
+        </div>
       ) : (
-        <Link to={""}>
+        <Link to={"/auth/sign-in"}>
           <Button>Get started</Button>
         </Link>
       )}
